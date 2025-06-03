4879d5752de5f2607b36d5339ae2974216dbafb2
Tycho Doorman <tycho26@kpnmail.nl>
2025-06-03 12:50:18 +0200

    clean slate.

--- WebUI/src/Settings.mjs
+++
@@ -1,9 +0,0 @@
-import data from "../settings.json"
-
-const SETTINGS = {
-    "Server IP": data["Server IP"],
-    "Server port": data["Server port"],
-    "First time": data["First time use"]
-}
-
-export default SETTINGS;
