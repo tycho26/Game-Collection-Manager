3639504b00cc5bc4fe5dc2439638bce5a91f0e19
Tycho Doorman <tycho26@kpnmail.nl>
2025-10-31 19:10:42 +0100

    Current state of game genres.

--- src/app/game/create/page.js
+++ src/app/game/create/page.js
@@ -1,85 +1,123 @@
-'use client'
+"use client";
 
 import {
-    Card,
-    CardAction,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/shadcn/ui/card"
-import { Input } from "@/components/shadcn/ui/input"
-import { Label } from "@/components/shadcn/ui/label"
-import { Button } from "@/components/shadcn/ui/button"
-import { createGame } from "@/serverActions"
-import { validateGame } from "@/validators/gameValidator"
-import { useActionState, useState } from "react"
-import { success } from "zod/v4"
-import FormInput from "@/components/ui/formInput"
-
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/shadcn/ui/card";
+import { Input } from "@/components/shadcn/ui/input";
+import { Label } from "@/components/shadcn/ui/label";
+import { Button } from "@/components/shadcn/ui/button";
+import { ScrollArea } from "@/components/shadcn/ui/scroll-area";
+import { createGame, getGameGenres } from "@/serverActions";
+import { validateGame } from "@/validators/gameValidator";
+import { useActionState, useState } from "react";
+import { success } from "zod/v4";
+import FormInput from "@/components/ui/formInput";
 
 export default function GameCreate() {
+  // TODO: Put into React state
+  let genresDOM = [];
+  getGameGenres().then((g) => {
+    genresDOM = g.map((genre) => <li key={genre.genre_id}>genre.name</li>);
+  });
 
-    const submitForm = function (currentState, formData) {
-        let data = Object.fromEntries(formData.entries())
-        data.gameRelease = new Date(data.gameRelease)
-        data.gameRating = Number(data.gameRating)
+  console.log(genresDOM);
+  const submitForm = function (currentState, formData) {
+    let data = Object.fromEntries(formData.entries());
+    data.gameRelease = new Date(data.gameRelease);
+    data.gameRating = Number(data.gameRating);
 
-
-        // - [x] zod errors parsen en in const doen
-        // - [x] treeify'en
-        // - [x] object maken met field name als key, en (eerste) error als value
-        // - [ ] maak labels in HTML form voor errors voor velden
-        // - [ ] assign value aan labels
-        // - [ ] if no errors, run create game server availableactionalue aan labels
-        // - [ ] if no errors, run create game server action
-        console.log(data)
-        const valReport = validateGame(data)
-        console.log(valReport)
-        let valSummary = {
-            success: true
-        }
-        if (!valReport.success) {
-            valSummary.success = false
-            Object.keys(valReport.errors.properties).forEach(key => valSummary[key] = valReport.errors.properties[key].errors[0])
-            console.log(valSummary)
-            return valSummary
-        }
-        else {
-            console.log("No validation errors!")
-            createGame(data);
-            return valSummary
-        }
-
-
+    // - [x] zod errors parsen en in const doen
+    // - [x] treeify'en
+    // - [x] object maken met field name als key, en (eerste) error als value
+    // - [ ] maak labels in HTML form voor errors voor velden
+    // - [ ] assign value aan labels
+    // - [ ] if no errors, run create game server availableactionalue aan labels
+    // - [ ] if no errors, run create game server action
+    console.log(data);
+    const valReport = validateGame(data);
+    console.log(valReport);
+    let valSummary = {
+      success: true,
+    };
+    if (!valReport.success) {
+      valSummary.success = false;
+      Object.keys(valReport.errors.properties).forEach(
+        (key) => (valSummary[key] = valReport.errors.properties[key].errors[0]),
+      );
+      console.log(valSummary);
+      return valSummary;
+    } else {
+      console.log("No validation errors!");
+      createGame(data);
+      return valSummary;
     }
+  };
 
-    const [formState, formAction, isPending] = useActionState(submitForm, {})
+  const [formState, formAction, isPending] = useActionState(submitForm, {});
 
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-6 mt-5 md:mt-20 justify-center">
-            <Card className="md:col-start-3 md:col-span-2">
-                <CardHeader>
-                    <CardTitle>Add game</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <form action={formAction}>
-                        <div className="md:inline-flex md:gap-2">
-                            <FormInput valError={formState.gameTitle} type="text" name="gameTitle" placeholder="Title" />
-                            <span className="m-1 md:m-0">
-                                <FormInput valError={formState.gameRelease} required type="date" name="gameRelease" placeholder="Release date" />
-                            </span>
-                        </div>
-                        <div className="mt-3parse md:inline-flex md:gap-2">
-                            <FormInput valError={formState.gameDev} type="text" name="gameDev" placeholder="Developer" />   
-                            <FormInput valError={formState.gamePub} type="text" name="gamePub" placeholder="Publisher" />
-                        </div>
-                            <FormInput valError={formState.gameRating} type="number" name="gameRating"  placeholder="Rating" />
-                        <Button className="mt-3" disabled={isPending}>Create</Button>
-                    </form>
-                </CardContent>
-            </Card>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-6 mt-5 md:mt-20 justify-center">
+      <Card className="md:col-start-3 md:col-span-2">
+        <CardHeader>
+          <CardTitle>Add game</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form action={formAction}>
+            <div className="md:inline-flex md:gap-2">
+              <FormInput
+                valError={formState.gameTitle}
+                type="text"
+                name="gameTitle"
+                placeholder="Title"
+              />
+              <span className="m-1 md:m-0">
+                <FormInput
+                  valError={formState.gameRelease}
+                  required
+                  type="date"
+                  name="gameRelease"
+                  placeholder="Release date"
+                />
+              </span>
+            </div>
+            <div className="mt-3 md:inline-flex md:gap-2">
+              <FormInput
+                valError={formState.gameDev}
+                type="text"
+                name="gameDev"
+                placeholder="Developer"
+              />
+              <FormInput
+                valError={formState.gamePub}
+                type="text"
+                name="gamePub"
+                placeholder="Publisher"
+              />
+            </div>
+            <div className="mt-3 md:inline-flex md:gap-2">
+              <ScrollArea className="h-50 w-50">
+                <h5 className="font-bold">Genres</h5>
+                <ul>{genresDOM}</ul>
+              </ScrollArea>
+            </div>
+            <FormInput
+              valError={formState.gameRating}
+              type="number"
+              name="gameRating"
+              placeholder="Rating"
+            />
+            <Button className="mt-3" disabled={isPending}>
+              Create
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- src/serverActions.js
+++ src/serverActions.js
@@ -1,22 +1,29 @@
-'use server'
+"use server";
 
 import { PrismaClient } from "../generated/prisma";
 
-export async function createGame(formData){
-    // const gameTitle = formData.get("gameTitle")
-    // const gameRelease = new Date(formData.get("gameRelease"))
-    // const gameDev = formData.get("gameDev")
-    // const gamePub = formData.get("gamePub")
-    // const gameRating = Number(formData.get("gameRating"))
-    const {gameTitle, gameRelease, gameDev, gamePub, gameRating} = formData;
-    const prisma = new PrismaClient
-    await prisma.games.create({
-        data:{
-            title:gameTitle,
-            release_date:gameRelease,
-            publisher:gamePub,
-            developer:gameDev,
-            rating:gameRating
-        }
-    })
+export async function getGameGenres() {
+  const prisma = new PrismaClient();
+  const genres = await prisma.genres.findMany();
+  console.log(genres);
+  return genres;
+}
+
+export async function createGame(formData) {
+  // const gameTitle = formData.get("gameTitle")
+  // const gameRelease = new Date(formData.get("gameRelease"))
+  // const gameDev = formData.get("gameDev")
+  // const gamePub = formData.get("gamePub")
+  // const gameRating = Number(formData.get("gameRating"))
+  const { gameTitle, gameRelease, gameDev, gamePub, gameRating } = formData;
+  const prisma = new PrismaClient();
+  await prisma.games.create({
+    data: {
+      title: gameTitle,
+      release_date: gameRelease,
+      publisher: gamePub,
+      developer: gameDev,
+      rating: gameRating,
+    },
+  });
 }
